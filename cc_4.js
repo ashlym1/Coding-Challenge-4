5685624fe699e833a4d946ed824388795486a7b9
ashlym1 <ashlym3@usf.edu>
2025-02-02 16:50:17 -0500

    Task 1 - Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,4 @@
-// Task 1 Customer Discounts
+// Task 1 If Statements
 let purchaseAmount = 104; // Total  purchase amount 
 let finalAmount = purchaseAmount;// Final amount after discount 
 if(purchaseAmount >100){
@@ -8,10 +8,18 @@ if(purchaseAmount >100){
 console.log("Discount Successfully Applied!");
 console.log(`Final Amount after discount: $${finalAmount}`);
 
-//Task 2 Sales Report
+//Task 2 For Loop 
 let sales =[ 140, 40, 85, 240, 60]; // array of sales 
 let totalSales = 0; 
 for (let i = 0;i < sales.length; i++) {
     totalSales += sales[i]; //calculate the total of sales 
 }
 console.log (`Total sales: $${totalSales}`);
+
+// Task 3 while loop
+let stock=10; //curent stock
+while(stock>0) { 
+    console.log(`Stock remaining: ${stock}`);
+    stock--;
+} // decresing stock to 0 
+console.log("Warning: Stock Reduced !");
